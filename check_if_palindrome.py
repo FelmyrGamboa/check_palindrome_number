65d8723ad768cc22d45bfe78453d4353a679b16f
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-23 01:24:43 +0800

    Created the function to check a number if palindrome and printing the result it will have

--- check_if_palindrome.py
+++ check_if_palindrome.py
@@ -12,8 +12,15 @@
 # No. given number is not palindrome number
 
 #Create a function to check whether it is a palindrome
+def check_if_palindrome(check_number):
 #Create a condition if the number is palindrome then print the result saying it is a palindrome
+    if check_number[::-1] == check_number:
+        print("Orginal number is", check_number)
+        print("Yes, given number is a palindrome number.")    
 #If not print the result saying it is not a palindrome
+    else:
+        print("Original number is", check_number)
+        print("No, given number is not a palindrome number.")
 
 #Create a variables with the given numbers
 check_number_1 = 121

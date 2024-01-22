03aa39f9cca5a427092b030677c052866669e11a
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-23 01:17:51 +0800

    Pseudocodes

--- check_if_palindrome.py
+++ check_if_palindrome.py
@@ -2,3 +2,20 @@
 # Write a program to check if the given number is a palindrome number.
 
 # A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+
+# Expected Output:
+
+# original number 121
+# Yes. given number is palindrome number
+
+# original number 125
+# No. given number is not palindrome number
+
+#Create a function to check whether it is a palindrome
+#Create a condition if the number is palindrome then print the result saying it is a palindrome
+#If not print the result saying it is not a palindrome
+
+#Create a variables with the given numbers
+
+#Call out the function
+
